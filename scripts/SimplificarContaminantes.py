9698f2ab5939fc43a280e5dffd6d06715b025cb5
Your Name <lc.marrufopadilla@gmail.com>
2025-09-05 15:13:14 -0600

    rutas correctas

--- scripts/SimplificarContaminantes.py
+++ scripts/SimplificarContaminantes.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("data/calidad_aire_2025.csv")
+df = pd.read_csv("../data/calidad_aire_2025.csv")
 
 df_sum = df.groupby(["Entidad", "Municipio"], as_index=False).sum(numeric_only=True)
-df_sum.to_csv("data_util/Emisiones_municipio.csv", index=False)
+df_sum.to_csv("../data_util/Emisiones_municipio.csv", index=False)
